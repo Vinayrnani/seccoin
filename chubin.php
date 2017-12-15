14ea69ca4673adcb2990917584f88066d548e093
Vinay <vinay784.nani@gmail.com>
2017-12-15 21:11:12 +0530

    coinhub 1

--- chubin.php
+++ chubin.php
@@ -1,6 +1,6 @@
 <?php
 
 $site_contents=file_get_contents('https://www.coinhubindia.com');
-echo substr($site_contents(strpos($site_contents,'<rate'),strpos($site_contents,'</rate'));
+echo substr($site_contents,strpos($site_contents,'<rate'),strpos($site_contents,'</rate'));
 
 ?>
